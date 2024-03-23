4c597699a1b30abe1bc97e35b682db958774e1d9
MD Rayhan Howlader <rhowlader5314@gmail.com>
2024-03-23 10:57:50 +0800

    json data has been write from json file

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const other = require('./other');
-
-
 const http = require('http');
+const other = require('./other');
+const user = require('./user.json');
+
+
 
 const server = http.createServer((req, res) => {
     if(req.url == '/'){
-        res.writeHead(200, {'Content-Type:' : 'text/html'})
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write('<p>This is Home page</p>');
+        res.end();
+    }
+    else if(req.url == '/contact'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write('<p>This is Contact page</p>');
+        res.end();
+    }
+    if(req.url == '/about'){
+        res.writeHead(200, {'Content-Type' : 'application/json'});
+        res.write(JSON.stringify(user));
+        res.end();
     }
 })
 
